8f0af581125c327a2f2a3fd143e279328830ce69
chinaguocg <chinaguocg@gmail.com>
2025-11-22 21:41:12 +0800

    update

--- docs/guide/sidebar.js
+++ docs/guide/sidebar.js
@@ -9,29 +9,16 @@ module.exports = [
         ]
     },
     {
-        title:'深入',
-        collapsable: true,
-        children:[
-            '/guide/depth/01',
-            '/guide/depth/02',
-            '/guide/depth/03',
-            '/guide/depth/04',
-            '/guide/depth/05',
-            '/guide/depth/06',
-
-        ]
-    },
-    {
         title:'思考',
         collapsable: true,
         children:[
+            '/guide/depth/01',
             '/guide/think/01',
             '/guide/think/02',
             '/guide/think/03',
             '/guide/think/04',
             '/guide/think/05',
             '/guide/think/06',
-            '/guide/think/07',
 
         ]
     },
